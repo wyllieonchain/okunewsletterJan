d2acb3c09099e25fb854aa6bbfa7c43cdc5b1300
wyllieonchain <45154279+wyllieonchain@users.noreply.github.com>
2024-02-19 19:55:03 +0000

    fix small bug

--- app/api/route.ts
+++ app/api/route.ts
@@ -28,7 +28,7 @@ const id = data.untrustedData.buttonIndex;
     <meta property="fc:frame" content="vNext" />
     <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_BASE_URL}/2.png" />
     <meta property="fc:frame:button:1" content="Restart" />
-    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api?$3" />
+    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api?id=3" />
   </head></html>`);
   } else {
     return new NextResponse(`<!DOCTYPE html><html><head>
