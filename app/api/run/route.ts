ae7dc6f909816be9d0b411195a5f8caefb3eca69
wyllieonchain <45154279+wyllieonchain@users.noreply.github.com>
2024-02-20 16:09:24 +0000

    changing the main route

--- app/api/run/route.ts
+++ app/api/run/route.ts
@@ -15,22 +15,30 @@ const id = data.untrustedData.buttonIndex;
 // have my two cases, route to the right one
   if(id === 1){
       return new NextResponse(`<!DOCTYPE html><html><head>
-    <title>Good</title>
+    <title>Multichain Volume</title>
     <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_BASE_URL}/1.png" />
-    <meta property="fc:frame:button:1" content="Still trade onchain" />
-    <meta property="fc:frame:button:2" content="I don't anymore" />
-    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/run?${id}" />
+    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_BASE_URL}/mvol.png" />
+    <meta property="fc:frame:button:1" content="Return" />
+    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/end?${id}" />
   </head></html>`);
   } else if(id === 2) {
   return new NextResponse(`<!DOCTYPE html><html><head>
-    <title>You suck</title>
+    <title>Layer1 Layer2</title>
     <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_BASE_URL}/02.png" />
-    <meta property="fc:frame:button:1" content="Restart" />
+    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_BASE_URL}/l1l2.png" />
+    <meta property="fc:frame:button:1" content="Return" />
     <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/end?${id}" />
   </head></html>`);
   }
+  } else if(id === 3) {
+    return new NextResponse(`<!DOCTYPE html><html><head>
+      <title>Protocol Efficiency</title>
+      <meta property="fc:frame" content="vNext" />
+      <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_BASE_URL}/peff.png" />
+      <meta property="fc:frame:button:1" content="Return" />
+      <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/end?${id}" />
+  </head></html>`);
+    }
 }
 
 export async function POST(req: NextRequest): Promise<Response> {
